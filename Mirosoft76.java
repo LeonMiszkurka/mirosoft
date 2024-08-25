0ee7d5d0db8638fedef4992922e20035ec07401a
Leon Miszkurka <leonmiszkurka@gmail.com>
2024-08-25 15:45:03 -0700

    mirosoft code

---
+++ Mirosoft76.java
@@ -0,0 +1,13 @@
+package Helloworld
+;
+
+import javax.swing.JOptionPane;
+
+public class Mirosoft76 {
+    
+    public static void main(String[] args) {
+    
+        JOptionPane.showInputDialog(null, args, "mirosoft", 0);
+        
+    }
+}
