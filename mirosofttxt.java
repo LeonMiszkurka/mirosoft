20d811663b22054f8576d2659ba681373637262a
Leon Miszkurka <leonmiszkurka@gmail.com>
2024-08-30 10:04:07 -0700

    mirosofttxt.java

---
+++ mirosofttxt.java
@@ -0,0 +1,15 @@
+
+package Helloworld
+;
+
+import javax.swing.JOptionPane;
+
+public class Mirosoft76 {
+    
+    public static void main(String[] args) {
+    
+        JOptionPane.showOptionDialog(null, args, "mirosoft", 0);
+        
+    }
+}
+
