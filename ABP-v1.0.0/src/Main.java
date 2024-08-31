617622a86af7508f0ca3da2b64a758eb2f8f3406
Alex <alexmis8@gmail.com>
2024-08-30 18:28:59 -0700

    Adding JFrame with loading button behaviour

--- ABP-v1.0.0/src/Main.java
+++ ABP-v1.0.0/src/Main.java
@@ -1,14 +1,37 @@
 
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class Main {
     public static void main(String[] args) {
         String input = JOptionPane.showInputDialog("ABPv1.0.0");
-        if (input == null) {
-            JOptionPane.showMessageDialog(null, "run.ABP=BOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+        if (input != null) {
+            JFrame jFrame = new JFrame("ButtonFrame");
+            JPanel jPanel = new JPanel();
+
+            JButton jButton = new JButton("LoadingButton");
+            jButton.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println("Loading...");
+                }
+                
+            });
+
+            jPanel.add(jButton);
+            jFrame.add(jPanel);
+
+            jFrame.setSize(300, 300);
+            jFrame.setVisible(true);
         } else {
-            JOptionPane.showMessageDialog(null, "ABPv1.0 has run: " + input);
+            JOptionPane.showMessageDialog(null, "Boo to you. Ending program");
         }
     }
 }
