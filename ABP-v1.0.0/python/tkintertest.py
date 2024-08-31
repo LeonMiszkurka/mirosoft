3981a3eac7760c583338ec962bcaddcdbe7f0c97
Alex <alexmis8@gmail.com>
2024-08-31 13:40:42 -0700

    adding python

---
+++ ABP-v1.0.0/python/tkintertest.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from math import *
+
+def evaluate(event):
+    res.configure(text="Result: " + str(eval(entry.get())))
+    x=0
+    while (bool(1)):
+        print(x)
+        x=x+1
+
+w = tk.Tk()
+w.geometry("500x200")
+tk.Label(w, text="Your Expression:").pack()
+entry = tk.Entry(w)
+entry.bind("<Return>",evaluate)
+entry.pack()
+res = tk.Label(w)
+res.pack()
+w.mainloop()
